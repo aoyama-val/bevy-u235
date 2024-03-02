12330062a48d61e7555f1d818a5dbfa2e6711d16
Aoyama, Shotaro <aoyama@val.co.jp>
2024-03-03 03:10:09 +0900

    プレイヤー移動

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,62 @@ const X_MAX: i32 = 640 / 16 - 3;
 const Y_MIN: i32 = 2;
 const Y_MAX: i32 = 400 / 16 - 3;
 
+// #[derive(Component)]
+// struct X(i32);
+// #[derive(Component)]
+// struct Y(i32);
+
+#[derive(Debug, Clone, PartialEq, Eq, Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+impl Position {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
+
+#[derive(Component)]
+struct Player;
+
+// #[derive(PartialEq, Eq)]
+// enum MoveEvent {
+//     Left,
+//     Right,
+// }
+
+fn move_player(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    let mut player_transform = query.single_mut();
+    // let mut direction = 0.0;
+
+    if keyboard_input.pressed(KeyCode::ArrowLeft) {
+        player_transform.translation.x = (player_transform.translation.x - CELL_SIZE_PX);
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowRight) {
+        player_transform.translation.x = (player_transform.translation.x + CELL_SIZE_PX);
+    }
+    player_transform.translation.x = player_transform.translation.x.clamp(
+        CELL_SIZE_PX * X_MIN as f32,
+        CELL_SIZE_PX * (X_MAX - 2) as f32,
+    );
+
+    // // Calculate the new horizontal paddle position based on player input
+    // let new_paddle_position =
+    //     paddle_transform.translation.x + direction * PADDLE_SPEED * time.delta_seconds();
+
+    // // Update the paddle position,
+    // // making sure it doesn't cause the paddle to leave the arena
+    // let left_bound = LEFT_WALL + WALL_THICKNESS / 2.0 + PADDLE_SIZE.x / 2.0 + PADDLE_PADDING;
+    // let right_bound = RIGHT_WALL - WALL_THICKNESS / 2.0 - PADDLE_SIZE.x / 2.0 - PADDLE_PADDING;
+}
+
 fn main() {
     App::new()
         .add_plugins((
@@ -43,21 +99,23 @@ fn main() {
         ))
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .add_systems(Startup, setup)
+        .add_systems(
+            FixedUpdate,
+            (
+                move_player,
+                // check_for_collisions,
+                // play_collision_sound,
+            )
+                .chain(),
+        )
         .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let mut transform = Transform::from_xyz(SCREEEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0, 999.0);
-    // transform.rotate(Quat::from_euler(
-    //     EulerRot::XYZ,
-    //     180.0f32.to_radians(),
-    //     0.0,
-    //     0.0,
-    // ));
-    // transform.with_translation (SCREEEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0, -999.0);
+    // Camera
     commands.spawn(Camera2dBundle {
-        transform: transform,
+        transform: Transform::from_xyz(SCREEEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0, 999.0),
         projection: OrthographicProjection {
             scaling_mode: bevy::render::camera::ScalingMode::WindowSize(1.0),
             ..default()
@@ -71,37 +129,31 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         ..Default::default()
     };
 
-    // render player
+    // Player
+    let player_position = Position::new(18, Y_MAX);
     let player_texture = asset_server.load("image/player.png");
-    commands.spawn(SpriteBundle {
-        texture: player_texture,
-        transform: Transform::from_xyz(
-            // 0.0,
-            // SCREEN_HEIGHT,
-            CELL_SIZE_PX * 18.0,
-            SCREEN_HEIGHT - CELL_SIZE_PX * Y_MAX as f32,
-            0.0,
-        ),
-        sprite: sprite.clone(),
-        ..default()
-    });
-    // commands.spawn(SpriteBundle {
-    //     texture: asset_server.load("image/target.png"),
-    //     transform: Transform::from_xyz(SCREEEN_WIDTH, SCREEN_HEIGHT / 2.0, 0.0),
-    //     ..default()
-    // });
+    commands.spawn((
+        SpriteBundle {
+            texture: player_texture,
+            transform: position_to_transform(Position::new(18, Y_MAX)),
+            sprite: sprite.clone(),
+            ..default()
+        },
+        player_position,
+        Player,
+    ));
 
-    // render wall
+    // Walls
     for y in 1..=Y_MAX {
         commands.spawn(SpriteBundle {
             texture: asset_server.load("image/wall.png"),
-            transform: cell_to_transform(1, y),
+            transform: position_to_transform(Position::new(1, y)),
             sprite: sprite.clone(),
             ..default()
         });
         commands.spawn(SpriteBundle {
             texture: asset_server.load("image/wall.png"),
-            transform: cell_to_transform(X_MAX + 1, y),
+            transform: position_to_transform(Position::new(X_MAX + 1, y)),
             sprite: sprite.clone(),
             ..default()
         });
@@ -109,35 +161,39 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     for x in 1..(X_MAX + 1) {
         commands.spawn(SpriteBundle {
             texture: asset_server.load("image/wall.png"),
-            transform: cell_to_transform(x, 1),
+            transform: position_to_transform(Position::new(x, 1)),
             sprite: sprite.clone(),
             ..default()
         });
     }
 
-    // render back
+    // Back
     for i in (X_MIN - 2)..=(X_MAX + 2) {
         commands.spawn(SpriteBundle {
             texture: asset_server.load("image/back.png"),
-            transform: cell_to_transform(i, Y_MAX + 1),
+            transform: position_to_transform(Position::new(i, Y_MAX + 1)),
             sprite: sprite.clone(),
             ..default()
         });
     }
 
-    // render title
+    // Title
     commands.spawn(SpriteBundle {
         texture: asset_server.load("image/title.png"),
-        transform: cell_to_transform(1, 0),
+        transform: position_to_transform(Position::new(1, 0)),
         sprite: sprite.clone(),
         ..default()
     });
+
+    // // Sound
+    // let ball_collision_sound = asset_server.load("sounds/breakout_collision.ogg");
+    // commands.insert_resource(CollisionSound(ball_collision_sound));
 }
 
-fn cell_to_transform(cx: i32, cy: i32) -> Transform {
+fn position_to_transform(position: Position) -> Transform {
     Transform::from_xyz(
-        CELL_SIZE_PX * cx as f32,
-        SCREEN_HEIGHT - CELL_SIZE_PX * cy as f32,
+        CELL_SIZE_PX * position.x as f32,
+        SCREEN_HEIGHT - CELL_SIZE_PX * position.y as f32,
         0.0,
     )
 }
